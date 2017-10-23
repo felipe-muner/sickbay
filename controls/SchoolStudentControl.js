8b16af02ff6502213291efb7e4a191bc9fcd5986
Jean Fíngolo Dias <jeanfingolodias@gmail.com>
2017-10-23 09:30:52 -0200

    remove min datetime in new attendance

--- controls/SchoolStudentControl.js
+++ controls/SchoolStudentControl.js
@@ -33,6 +33,35 @@ const query = "SELECT es.aluno.matricula, " +
               ") " +
               "ORDER BY es.aluno.primnomealuno||' '||COALESCE(es.aluno.segnomealuno, '')||' '|| es.aluno.ultnomealuno"
 
+const newQuery = "SELECT es.aluno.Matricula AS MATRICULA, " +
+                     "es.aluno.primnomealuno||' '||COALESCE(es.aluno.segnomealuno, '')||' '|| es.aluno.ultnomealuno AS NOME, " +
+                     "es.aluno.alergia AS ALERGIA, " +
+                     "COALESCE(es.turmaano.descturma, es.turma.descturma) AS TURMA, " +
+                     "es.serie.Descserie AS SERIE, " +
+                     "es.filial.Descfilial AS FILIAL, " +
+                     "es.filial.codfilial AS CODFILIAL " +
+                "FROM es.aluno " +
+                  "INNER JOIN es.alunoano ON (es.alunoano.matricula = es.aluno.matricula AND es.alunoano.anoletivo = '2017 ') " +
+                  "INNER JOIN es.turma ON (es.alunoano.codturma = es.turma.codturma) " +
+                  "LEFT JOIN es.turmaano ON (es.turmaano.codturma = es.turma.codturma AND es.turmaano.anoletivo = es.alunoano.anoletivo) " +
+                  "INNER JOIN es.serie ON (es.serie.codserie = es.turma.codserie) " +
+                  "INNER JOIN es.filial ON (es.filial.codfilial = es.turma.codfilialturma) " +
+                "WHERE es.alunoano.Datamatricula <= CURRENT_DATE " +
+                  "AND (COALESCE((SELECT es.alunostatus.statusaluno " +
+                                  "FROM (SELECT es.alunostatus.matricula, " +
+                                              "MAX(es.alunostatus.DATASTATUS) DATASTATUS, " +
+                                              "MAX(CAST( es.alunostatus.DATALANCSTATUS||' '||es.alunostatus.HORALANCSTATUS AS TIMESTAMP))DATA_HoraLANCSTATUS  " +
+                                          "FROM es.alunostatus " +
+                                          "WHERE  es.alunostatus.datastatus <= CURRENT_DATE " +
+                                            "AND CODUSUARIOESTORNO IS NULL " +
+                                          "GROUP BY es.alunostatus.matricula) x  " +
+                                    "INNER JOIN es.alunostatus " +
+                                      "ON (es.alunostatus.matricula = x.matricula AND " +
+                                        "es.alunostatus.DATASTATUS = x.DATASTATUS AND " +
+                                        "CAST( es.alunostatus.DATALANCSTATUS||' '||es.alunostatus.HORALANCSTATUS AS TIMESTAMP) = x.DATA_HoraLANCSTATUS)  " +
+                                    "WHERE es.alunostatus.matricula = es.aluno.matricula), '0') IN ('0', '4')) " +
+                "ORDER BY es.filial.Descfilial, es.aluno.Matricula"
+
 function doRelease(connection) {
   connection.close(function(err) {
     if(err) {
